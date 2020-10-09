eb5a04b6f9bb566de8c57d6919af91316b88f55d
Adam McCallum <adammccallum96@gmail.com>
2020-10-09 06:33:04 -0700

    Added model class and javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"com.csis3275.model_amc_37","l":"Adam"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.csis3275.model_amc_37","l":"GroupDescription_amc_37"}]
---
+++ src/main/java/com/csis3275/model_amc_37/Adam.java
@@ -0,0 +1,33 @@
+package com.csis3275.model_amc_37;
+
+public class Adam {
+
+	private String firstName;
+	private String lastName;
+	private String age;
+	private String favouriteColour;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getFavouriteColour() {
+		return favouriteColour;
+	}
+	public void setFavouriteColour(String favouriteColour) {
+		this.favouriteColour = favouriteColour;
+	}
+}
